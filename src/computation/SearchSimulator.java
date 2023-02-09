5c465bc20d6ef7ee10fd935c72aaf33c56d8984c
Arunanthi <arunantg@stud.ntnu.no>
2023-02-09 11:42:45 +0100

    Fixed some problems with SearchSimulator.java

--- src/computation/SearchSimulator.java
+++ src/computation/SearchSimulator.java
@@ -7,16 +7,59 @@ import utils.ResponseGenerator;
 import java.net.Socket;
 
 public class SearchSimulator {
+    /**
+     * simulates a search
+     * logs down the time when the request was recieved, print it out to the console
+     * waits a bit to simulate a search - using Thread.sleep();
+     * then logs down the time when the request was finished.
+     * Generates a header and html response, then writes it to the output stream.
+     * prints out when the search request finished.
+     * @param clientSocket
+     * @throws Exception
+     */
     public static void processClientRequest(Socket clientSocket) throws Exception {
+
+        InputStream  input  = clientSocket.getInputStream();
+        OutputStream output = clientSocket.getOutputStream();
         long time1 = System.currentTimeMillis();
-        System.out.println("Request processing started at: " + time1);
-        Thread.sleep(10 * 1000);
+        System.out.println("Request Recieved: " + time1);
+        // wait some time to simulate work, then resume
+        Thread.sleep(10*1000);
+
         long time2 = System.currentTimeMillis();
-        System.out.println("Request processing ended at: " + time2);
-        System.out.println(ResponseGenerator.generatorResponseHTML(time1,time2));
-        System.out.println(ResponseGenerator.generatorResponseHeader(ResponseGenerator.generatorResponseHTML(time1,time2).length()));
+
+        byte[] response = ResponseGenerator.generatorResponseHTML(time1, time2).getBytes("UTF-8");
+        byte[] header = ResponseGenerator.generatorResponseHeader(response.length).getBytes("UTF-8");
+        output.write(header);
+        output.write(response);
+        output.close();
+        input.close();
 
 
-
+        System.out.println("Request processed: " + time2);
+//        InputStream  input  = clientSocket.getInputStream();
+//        OutputStream output = clientSocket.getOutputStream();
+//        long time1 = System.currentTimeMillis();
+//
+//        byte[] responseDocument = ("<html><body>" +
+//                "Singlethreaded Server: " +
+//                time1 +
+//                "</body></html>").getBytes("UTF-8");
+//
+//        byte[] responseHeader =
+//                ("HTTP/1.1 200 OK\r\n" +
+//                        "Content-Type: text/html; charset=UTF-8\r\n" +
+//                        "Content-Length: " + responseDocument.length +
+//                        "\r\n\r\n").getBytes("UTF-8");
+//
+//        long time2 = System.currentTimeMillis();
+//        output.write(responseHeader);
+//        output.write(responseDocument);
+//        output.close();
+//        input.close();
+//
+//        System.out.println("Request processed: " + time2);
     }
+
+
 }
