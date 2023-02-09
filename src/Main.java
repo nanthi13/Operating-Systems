9331eec26353756f748ef90e00363cb0de4dbb39
Arunanthi <arunantg@stud.ntnu.no>
2023-02-09 11:40:00 +0100

    fixed @SearchSimulator.java. Now properly writes out header and html message

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,8 @@ import java.net.Socket;
 public class Main {
 
     public static void main(String[] args) {
-        SearchSimulator search = new SearchSimulator();
         SingleThreadedServer server = new SingleThreadedServer(9000);
+
 
         new Thread(server).start();
 
